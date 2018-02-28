04061c0381dc72aee69a1cf40847df851de45581
SprinklR <sprinkl@yandex.ru>
2018-02-28 20:58:30 +0300

    Create device manager

---
+++ devicemanager.cpp
@@ -0,0 +1,6 @@
+#include "devicemanager.h"
+
+deviceManager::deviceManager()
+{
+
+}
---
+++ devicemanager.h
@@ -0,0 +1,11 @@
+#ifndef DEVICEMANAGER_H
+#define DEVICEMANAGER_H
+
+
+class deviceManager
+{
+public:
+    deviceManager();
+};
+
+#endif // DEVICEMANAGER_H
