f9aceb2978dacbf40e54f346f7ad78ce59c3825f
SprinklR <sprinkl@yandex.ru>
2018-03-04 22:06:08 +0300

    Add code

--- devicemanager.cpp
+++ devicemanager.cpp
@@ -1,6 +1,26 @@
 #include "devicemanager.h"
 
-deviceManager::deviceManager()
+DeviceManager::DeviceManager()
 {
 
 }
+
+void DeviceManager::addDevice()
+{
+    matrConn_.resize(matrConn_.getRows() + 1);
+}
+
+void DeviceManager::addConnection(int sourceIdDevice,
+                                  int finalDivece, int destIdDevice)
+{
+    matrConn_[sourceIdDevice][finalDivece] = destIdDevice;
+}
+
+void DeviceManager::removeDevice(int idDevice)
+{
+    for (int i = 0; i < matrConn_.getRows(); i++)
+        matrConn_[idDevice][i] = 0;
+
+    matrConn_.removeAt(idDivice);
+}
+
--- devicemanager.h
+++ devicemanager.h
@@ -1,11 +1,20 @@
 #ifndef DEVICEMANAGER_H
 #define DEVICEMANAGER_H
+#include "matrix.h"
 
-
-class deviceManager
+class DeviceManager
 {
 public:
-    deviceManager();
+    DeviceManager();
+
+    void addDevice();
+    void addConnection(int sourceIdDevice, int finalDivece, int destIdDevice);
+    void removeDevice(int idDevice);
+
+private:
+    Matrix<int> matrConn_;
+
+
 };
 
 #endif // DEVICEMANAGER_H
